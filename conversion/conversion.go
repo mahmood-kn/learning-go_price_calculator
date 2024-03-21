98a265ea34ec1966261104ef4eb2515e2382d411
Mahmood <mahmood.kn1526@gmail.com>
2024-03-21 21:29:04 +0330

    9-8 add conversion

---
+++ conversion/conversion.go
@@ -0,0 +1,18 @@
+package conversion
+
+import (
+	"errors"
+	"strconv"
+)
+
+func StringToFloat(strings []string) ([]float64, error) {
+	var floats []float64
+	for _, stringVal := range strings {
+		floatVal, err := strconv.ParseFloat(stringVal, 64)
+		if err != nil {
+			return nil, errors.New("fail to convert string to float")
+		}
+		floats = append(floats, floatVal)
+	}
+	return floats, nil
+}
--- prices/prices.go
+++ prices/prices.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+
+	"example.com/price-calculator/conversion"
 )
 
 type TaxIncludedPriceJob struct {
@@ -34,18 +35,14 @@ func (job *TaxIncludedPriceJob) LoadData() {
 		file.Close()
 		return
 	}
-	prices := make([]float64, len(lines))
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-		if err != nil {
-			fmt.Println("Converting price to float failed")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
+	prices, err := conversion.StringToFloat(lines)
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
 	}
 	job.InputPrices = prices
+	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
